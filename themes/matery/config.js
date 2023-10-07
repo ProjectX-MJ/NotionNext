e7056b386579f401633bd0be9dfec9fde438392a
ProjectX-MJ <59431322+ProjectX-MJ@users.noreply.github.com>
2023-10-07 16:22:12 +0800

    Update config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  HOME_BANNER_ENABLE: false,
+  HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HOME_BANNER_GREETINGS: ['探索更多的可能性'], // 首页大图标语文字
 
